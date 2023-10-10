c7a422d556bea0824536a28720e0674eb3a8b955
Saurabh Kulkarni <saurabhk201@gmail.com>
2023-10-10 13:50:57 +0530

    Update server.js 505

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello from git via jenkins! Its working, but this time it might fail on prod env');
+    res.send('Hello from git via jenkins! Its working, but this time it might fail on prod env because now i am checking with default values');
 });
 
 app.listen(PORT, () => {
