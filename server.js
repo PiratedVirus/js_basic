0d57475d8610a5fdb64ce2f77a219965c8be94e4
Saurabh Kulkarni <saurabhk201@gmail.com>
2023-10-10 19:59:42 +0530

    updated db collections

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Its working, but this time it might fail on prod env because now i am checking with default values along with mongo db. Lets see if this commit reflects');
+    res.send('Hello! Its working, but this time it might fail on prod env because now i am checking with default values along with mongo db. Lets see if this commit reflects');
 });
 
 app.listen(PORT, () => {
