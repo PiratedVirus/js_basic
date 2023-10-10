9f51c31c78d50614932d78c52494d8a84d14f3b7
Saurabh Kulkarni <saurabhk201@gmail.com>
2023-10-10 18:31:45 +0530

    calling mongo.db

--- server.js
+++ server.js
@@ -3,21 +3,10 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    res.send('Hello from git via jenkins! Its working, but this time it might fail on prod env because now i am checking with default values');
+    res.send('Hello from git via jenkins! Its working, but this time it might fail on prod env because now i am checking with default values along with mongo db');
 });
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
 });
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/';
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, db) => {
-    if (err) throw err;
-
-    // Create a new database named "mydatabase"
-    const dbo = db.db("mydatabase");
-
-    console.log("Database created!");
-    db.close();
-});
